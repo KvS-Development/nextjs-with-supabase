febbd88ee697d882e6465c971fddce0c546164e2
VincentVans <VincentVans@users.noreply.github.com>
2025-11-29 14:19:07 +0100

    Path was incorrect

--- config/testing/playwright.config.ts
+++ config/testing/playwright.config.ts
@@ -14,7 +14,7 @@ const baseURL = process.env.BASE_URL || 'http://localhost:3000';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: '../../tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
